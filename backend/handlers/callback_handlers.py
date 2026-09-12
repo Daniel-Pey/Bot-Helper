b503b1dfc3609a4292c23ace5f879069bffff3a8
Danya <danyaputilin@yandex.ru>
2026-09-12 15:02:00 +0300

    Update callback_handlers.py

--- backend/handlers/callback_handlers.py
+++ backend/handlers/callback_handlers.py
@@ -4,7 +4,7 @@ from telebot import types
 from backend.keyboards import social_networks_keyboard
 
 
-bot.callback_query_handler(func=lambda call: call.data == "anon_message")
+@bot.callback_query_handler(func=lambda call: call.data == "anon_message")
 def anon_message_callback_handler(call):
     """Отправка анонимных сообщений админу"""
 
@@ -12,6 +12,6 @@ def anon_message_callback_handler(call):
     bot.register_next_step_handler(msg, callback=send_anon_message_to_admin)
 
 
-bot.callback_query_handler(func=lambda call: call.data == "social_networks")
+@bot.callback_query_handler(func=lambda call: call.data == "social_networks")
 def social_networks(call):
     bot.send_message(call.message.from_user.id, 'Наши социальные сети 👇', reply_markup=social_networks_keyboard())
