c43a111af1425e496df050e8281a4dd63f8fe8e6
Danya <danyaputilin@yandex.ru>
2026-09-12 15:06:07 +0300

    Update callback_handlers.py

--- backend/handlers/callback_handlers.py
+++ backend/handlers/callback_handlers.py
@@ -8,10 +8,10 @@ from backend.keyboards import social_networks_keyboard
 def anon_message_callback_handler(call):
     """Отправка анонимных сообщений админу"""
 
-    msg = bot.send_message(call.message.from_user.id, "Отлично! Напиши сообщение, которое я должен передать Дане:")
+    msg = bot.send_message(call.from_user.id, "Отлично! Напиши сообщение, которое я должен передать Дане:")
     bot.register_next_step_handler(msg, callback=send_anon_message_to_admin)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "social_networks")
 def social_networks(call):
-    bot.send_message(call.message.from_user.id, 'Наши социальные сети 👇', reply_markup=social_networks_keyboard())
+    bot.send_message(call.from_user.id, 'Наши социальные сети 👇', reply_markup=social_networks_keyboard())
