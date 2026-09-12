f02319309e8bc868dc1708b3604b4c38999be9b1
Danya <danyaputilin@yandex.ru>
2026-09-12 15:12:19 +0300

    Update admin_commands.py

--- backend/handlers/admin_commands.py
+++ backend/handlers/admin_commands.py
@@ -2,14 +2,17 @@ from backend.bot import bot
 from backend.config import config
 
 
-@bot.message_handler(func=lambda x: x.from_user.id == config.ADMIN_ID and x.reply_to_message)
+@bot.message_handler(func=lambda x: x.from_user.id == config.ADMIN_ID)
 def admin_answer_for_anon(message):
     """Ответ админа на анонимное сообщение"""
 
-    replied_message = message.reply_to_message.text
-    anon_id = replied_message[:replied_message.index('\n')]
-    try:
-        bot.send_message(anon_id, "Ответ от Дани:\n" + message.text)
-        bot.send_message(config.ADMIN_ID, "✅ Ваше сообщение отправлено")
-    except Exception as e:
-        bot.send_message(config.ADMIN_ID, f"❌ Не получилось ответить на сообщение:\n{e}")
+    if message.reply_to_message:
+        replied_message = message.reply_to_message.text
+        anon_id = replied_message[:replied_message.index('\n')]
+        try:
+            bot.send_message(anon_id, "Ответ от Дани:\n" + message.text)
+            bot.send_message(config.ADMIN_ID, "✅ Ваше сообщение отправлено")
+        except Exception as e:
+            bot.send_message(config.ADMIN_ID, f"❌ Не получилось ответить на сообщение:\n{e}")
+    else:
+        bot.send_message(config.ADMIN_ID, 'Bla')
