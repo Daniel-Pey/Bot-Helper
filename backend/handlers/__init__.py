0cb6dc65881bbb08755f7b4b12d9a784c332d4da
Danya <danyaputilin@yandex.ru>
2026-09-12 16:58:30 +0300

    Update __init__.py

--- backend/handlers/__init__.py
+++ backend/handlers/__init__.py
@@ -3,8 +3,8 @@ from .groups import *
 from .channel_posts import *
 from .all_chats import *
 from .callback_handlers import *
-from .admin_commands import *
 from .private import *
+from .admin_commands import *
 
 """Обработчки сообщений"""
 
